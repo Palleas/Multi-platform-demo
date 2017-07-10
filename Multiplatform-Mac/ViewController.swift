4df81e44d1ca5f64f95943e381efbf23f8b66358
Romain Pouclet <romain.pouclet@gmail.com>
2017-07-10 10:07:34 -0400

    Add tvOS and macOS platforms

---
+++ Multiplatform-Mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Multiplatform-Mac
+//
+//  Created by Romain Pouclet on 2017-07-10.
+//  Copyright © 2017 Buddybuild. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
